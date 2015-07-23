99ddf395810c3f77ffbf22a8e732f25356ccb1e8
storm4430@yandex.ru <storm4430@yandex.ru>
2015-07-23 21:03:29 +0700

    Initial commit

---
+++ prospekt.tel/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(prospekt.tel.Startup))]
+namespace prospekt.tel
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
