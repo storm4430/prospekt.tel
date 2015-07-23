25c1fa7a6fb7a4abd33cae7da579f20639002284
storm4430@yandex.ru <storm4430@yandex.ru>
2015-07-23 21:50:55 +0700

    Адаптация UserModel

--- prospekt.tel/Models/IdentityModels.cs
+++ prospekt.tel/Models/IdentityModels.cs
@@ -1,14 +1,19 @@
-﻿using System.Data.Entity;
-using System.Security.Claims;
+﻿using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 
 namespace prospekt.tel.Models
 {
     // Чтобы добавить данные профиля для пользователя, можно добавить дополнительные свойства в класс ApplicationUser. Дополнительные сведения см. по адресу: http://go.microsoft.com/fwlink/?LinkID=317594.
     public class ApplicationUser : IdentityUser
     {
+        public Guid orgId { get; set; }
+        public string userFam { get; set; }
+        public string userIm { get; set; }
+        public string userOt { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Обратите внимание, что authenticationType должен совпадать с типом, определенным в CookieAuthenticationOptions.AuthenticationType
